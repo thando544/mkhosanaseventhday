adf912c346909f114b35cc4171259f07e2816a14
thando <mpofu7085@gmail.com>
2025-06-30 16:11:56 +0200

    about page added

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,13 +1,27 @@
-
 import React from "react";
 import { Link } from "react-router";
 
-function About(){
-  return(
-    <div>
-      hello about page
-    </div>
-  )
+function About() {
+  return (
+    <main>
+      {/* banner */}
+      <section></section>
+      {/* second section */}
+      <section></section>
+
+      {/* third section */}
+      <section></section>
+
+      {/* fourth section */}
+      <section></section>
+
+      {/* fifth section */}
+      <section></section>
+
+      {/* sixthy section */}
+      <section></section>
+    </main>
+  );
 }
 
 export default About;
